f521e6f37995dae26b54df2fe7cfe79e4e6ca69c
Davi <davilinfo@gmail.com>
2021-09-30 10:17:12 -0300

    version 1 site

---
+++ site/chains_of_foods/src/components/Header.js
@@ -0,0 +1,36 @@
+import React from "react";
+import '../css/bootstrap.css';
+import '../css/fonts.css';
+import '../css/style.css';
+
+function Header() {
+  return (            
+    <header className="page-header">
+        <div className="rd-navbar-wrap">  
+            <nav className="rd-navbar rd-navbar-classic rd-navbar-original rd-navbar-static rd-navbar--is-stuck">
+                <div className="rd-navbar-main-outer">
+                <div className="rd-navbar-main">                
+                    <div className="rd-navbar-panel">                   
+                    <button className="rd-navbar-toggle" data-rd-navbar-toggle=".rd-navbar-nav-wrap"><span></span></button>                  
+                    <div className="rd-navbar-brand"><a href="/"><img className="brand-logo-light" src="images/logo-default1-140x57.png" alt="" width="140" height="57" srcset="images/logo-default-280x113.png 2x"/></a></div>
+                    </div>
+                    <div className="rd-navbar-main-element">
+                    <div className="rd-navbar-nav-wrap">                    
+                        <ul className="rd-navbar-nav">
+                        <li className="rd-nav-item"><a className="rd-nav-link" href="/">Home</a>
+                        </li>
+                        <li className="rd-nav-item"><a className="rd-nav-link" href="/About">About</a>
+                        </li>                      
+                        <li className="rd-nav-item"><a className="rd-nav-link" href="/Contact">Contacts</a>
+                        </li>
+                        </ul><a className="button button-white button-sm" href="#">order now</a>
+                    </div>
+                    </div><a className="button button-white button-sm" href="#">order now</a>
+                </div>
+                </div>
+            </nav>
+        </div>
+    </header> 
+  );
+}
+export default Header;
