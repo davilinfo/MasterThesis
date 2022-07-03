bd7531c079209a476a7b648895d4191f8f40bd9a
Davi Alves <davilinfo@gmail.com>
2022-07-03 18:08:07 +0000

    n1

---
+++ site/administration/Information/Application/Services/ProxyRestaurantSidechainNode.cs
@@ -0,0 +1,35 @@
+﻿using Application.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+   public class ProxyRestaurantSidechainNode : IProxy, IProxyRestaurantSidechainNode
+   {
+      private readonly string apiDelegates = "/api/delegates";
+      private readonly string apiNodeInfo = "/api/node/info";
+      public ProxyRestaurantSidechainNode(ILogger<IProxy> logger, IConfiguration configuration) : base(logger, configuration, configuration.GetSection("SidechainNode:Node1").Value)
+      {
+         logger.LogInformation("IProxyRestaurantSidechain request initiating");         
+      }
+
+      public async Task<dynamic> GetSidechainDelegates()
+      {
+         var result = await GetAsync<dynamic>(apiDelegates);
+
+         return result;
+      }
+
+      public async Task<dynamic> GetSidechainNodeInfo()
+      {
+         var result = await GetAsync<dynamic>(apiNodeInfo);
+
+         return result;
+      }
+   }
+}
