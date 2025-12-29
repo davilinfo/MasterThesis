d6ba776eef7e71473241e8f86e8d26183b012808
Davi <davilinfo@gmail.com>
2025-12-29 13:45:55 -0300

    useEffect deallocation

--- site/chains_of_foods/src/App.js
+++ site/chains_of_foods/src/App.js
@@ -40,7 +40,7 @@ function App() {
     }    
 
     loadFoods();    
-  }, []);
+  }, foods);
 
   async function loadTopCryptos(){
 	    fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
